0c7bc7f3142408fa0f1b600b0e59af2b9ff865c2
tealro <39542562+tealrex@users.noreply.github.com>
2022-03-02 23:59:09 -0600

    meepmeep testing

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,78 +1,61 @@
 package com.example.meepmeeptesting;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.constraints.AngularVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationConstraint;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedLight;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.util.Arrays;
+
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(1600);
+        MeepMeep meepMeep = new MeepMeep(800);
 
         // Declare our first bot
-        RoadRunnerBotEntity myFirstBot = new DefaultBotBuilder(meepMeep)
-                // We set this bot to be blue
-                .setColorScheme(new ColorSchemeBlueDark())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .build()
-                );
-
-        // Declare out second bot
-        RoadRunnerBotEntity mySecondBot = new DefaultBotBuilder(meepMeep)
-                // We set this bot to be red
+        RoadRunnerBotEntity redWarehouseBot = new DefaultBotBuilder(meepMeep)
                 .setColorScheme(new ColorSchemeRedDark())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(30, 30, Math.toRadians(60), Math.toRadians(60), 11)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(30, 30, Math.toRadians(180)))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(12, -63, 0))
+                                .lineToSplineHeading(new Pose2d(36, -63, Math.toRadians(0)))
+                                .lineToSplineHeading(new Pose2d(50, -63, Math.toRadians(0)),
+                                        new MinVelocityConstraint(Arrays.asList(
+                                                new AngularVelocityConstraint(30),
+                                                new MecanumVelocityConstraint(10, 11)
+                                        )),
+                                        new ProfileAccelerationConstraint(30)
+                                )
+//                                .splineTo(new Vector2d(12, -63), Math.toRadians(0))
                                 .build()
                 );
 
-        // Declare out third bot
-        RoadRunnerBotEntity myThirdBot = new DefaultBotBuilder(meepMeep)
-                // We set this bot to be red
-                .setColorScheme(new ColorSchemeRedLight())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+        RoadRunnerBotEntity blueWarehouseBot = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(30, 30, Math.toRadians(60), Math.toRadians(60), 11)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-60, -60, Math.toRadians(90)))
-                                .forward(80)
-                                .turn(Math.toRadians(180))
-                                .forward(80)
-                                .turn(Math.toRadians(180))
-                                .forward(80)
-                                .turn(Math.toRadians(180))
-                                .forward(80)
-                                .turn(Math.toRadians(180))
-                                .build()
+                                drive.trajectorySequenceBuilder(new Pose2d(50, 45, 0))
+                                        .setReversed(true)
+//                                        .splineToConstantHeading(new Vector2d(40, 63), Math.toRadians(0))
+                                        .lineToLinearHeading(new Pose2d(40, 63, Math.toRadians(0)))
+//                                        .splineToConstantHeading(new Vector2d(12, 63), Math.toRadians(0))
+                                        .lineToLinearHeading(new Pose2d(12, 63, Math.toRadians(0)))
+                                        .build()
                 );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
 
-                // Add both of our declared bot entities
-                .addEntity(myFirstBot)
-                .addEntity(mySecondBot)
-                .addEntity(myThirdBot)
+                // Add bot entities
+                .addEntity(redWarehouseBot)
+                .addEntity(blueWarehouseBot)
                 .start();
     }
 }
