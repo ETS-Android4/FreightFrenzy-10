9ff027b7bf5a1519b864b480a407fefe4984ecfd
tealro <39542562+tealrex@users.noreply.github.com>
2022-04-11 16:52:40 -0500

    teleop changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Actuators.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Actuators.java
@@ -62,7 +62,7 @@ public class Actuators {
     private PIDController slidesController;
     private PIDController intakeController;
 
-    public static PIDCoefficients TURRET_COEFFICIENTS = new PIDCoefficients(0.0012, 0, 0);
+    public static PIDCoefficients TURRET_COEFFICIENTS = new PIDCoefficients(0.0018, 0, 0);
     public static PIDCoefficients SLIDES_COEFFICIENTS = new PIDCoefficients(0.002, 0, 0);
     public static PIDCoefficients INTAKE_COEFFICIENTS = new PIDCoefficients(0.005, 0, 0.0001);
 
